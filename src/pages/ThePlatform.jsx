ff6d77538137130fc5df09068a549fb02dc3a032
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-11-17 22:10:16 +0100

    feat: add Platform page

---
+++ src/pages/ThePlatform.jsx
@@ -0,0 +1,12 @@
+import BackButton from "../components/BackButton";
+
+const ThePlatform = () => {
+    return (
+        <>
+        <div>En plattform för er</div>
+        <BackButton />
+        </>
+    )
+}
+
+export default ThePlatform;
