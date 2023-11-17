ce2b606c8d72446b865af159b94f38625d72768d
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-11-17 22:12:29 +0100

    refactor: code clean and renaming files

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Advice from './pages/Advice';
+import BodyDamage from './pages/BodyDamage';
+import Environment from './pages/Environment';
+import ExpensiveHabit from './pages/ExpensiveHabit';
+import Help from './pages/Help';
+import NotFresh from './pages/NotFresh';
+import ThePlatform from './pages/ThePlatform';
+
+const App = () => {
+  return (
+  <Routes>
+    <Route path='/' element={<Home />}/>
+    <Route path='/tips&rad' element={<Advice />}/>
+    <Route path='/skadapakropp' element={<BodyDamage />}/>
+    <Route path='/miljopaverkan' element={<Environment />}/>
+    <Route path='/dyrt' element={<ExpensiveHabit />}/>
+    <Route path='/hjalp' element={<Help />}/>
+    <Route path='/intefresh' element={<NotFresh />}/>
+    <Route path='/plattform' element={<ThePlatform />}/>
+  </Routes>
+  );
+}
+
+export default App;
