c4e55c3e6bdfc6f44b90bb2c574069709cb6db18
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-11-17 22:10:45 +0100

    feat: add Help page

---
+++ src/pages/Help.jsx
@@ -0,0 +1,12 @@
+import BackButton from "../components/BackButton";
+
+const Help = () => {
+    return (
+        <>
+        <div>Hjälp en vän</div>
+        <BackButton />
+        </>
+    )
+}
+
+export default Help;
