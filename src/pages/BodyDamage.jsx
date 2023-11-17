97174a5a7e25ad9c9d98e569361ef11161dfd80a
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-11-17 22:11:18 +0100

    feat: add BodyDamage page

---
+++ src/pages/BodyDamage.jsx
@@ -0,0 +1,12 @@
+import BackButton from '../components/BackButton';
+
+const BodyDamage = () => {
+    return (
+        <>
+            <div>Onödig skada på kroppen</div>
+            <BackButton />
+        </>
+    )
+}
+
+export default BodyDamage;
