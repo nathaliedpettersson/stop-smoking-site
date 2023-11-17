d29580f5d39df3aebd9f9c0cb119265863e09261
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-11-17 22:10:29 +0100

    feat: add NotFresh page

---
+++ src/pages/NotFresh.jsx
@@ -0,0 +1,12 @@
+import BackButton from "../components/BackButton";
+
+const NotFresh = () => {
+    return (
+        <>
+        <div>Inte fresssh</div>
+        <BackButton />
+        </>
+    )
+}
+
+export default NotFresh;
