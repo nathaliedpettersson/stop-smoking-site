e8c90e9b8845882d80e3a15b7f3e509e97352276
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-11-17 22:11:09 +0100

    feat: add Environment page

---
+++ src/pages/Environment.jsx
@@ -0,0 +1,12 @@
+import BackButton from "../components/BackButton";
+
+const Environment = () => {
+    return (
+        <>
+        <div>Miljöpåverkan & barnarbete</div>
+        <BackButton />
+        </>
+    )
+}
+
+export default Environment;
