3a3821439f2becca8e851c8372c8b4b9e766b2c6
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-11-17 22:10:57 +0100

    feat: add ExpensiveHabit page

---
+++ src/pages/ExpensiveHabit.jsx
@@ -0,0 +1,12 @@
+import BackButton from "../components/BackButton";
+
+const ExpensiveHabit = () => {
+    return (
+        <>
+        <div>En dyr vana</div>
+        <BackButton />
+        </>
+    )
+}
+
+export default ExpensiveHabit;
