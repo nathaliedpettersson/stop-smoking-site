6b13f6654fb3aad18eb71d54fd258c5ba374d450
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-11-24 12:14:04 +0100

    refactor: change styling for BackButton

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const BackButton = () => {
     return (
-<Link to='/' style={{fontSize: '20px'}}>Back</Link>
+<Link to='/' style={{fontSize: '50px', textDecoration: 'none', margin: '0.7rem'}}>⬅️</Link>
     )
 }
 
